5202b56246ea79980ebf9bccb5e127a8408d5b70
quabwww <josuebuenovargas@gmail.com>
2024-10-05 19:52:36 -0500

    d

--- MusicaBot/audio.py
+++ MusicaBot/audio.py
@@ -16,15 +16,21 @@ def get_youtube_audio_url(video_url):
             audio_url = info_dict.get("url", None)
 
             if not audio_url:
-                # Si no se encuentra la URL, lista los formatos disponibles
                 formats = info_dict.get('formats', None)
                 if formats:
                     for f in formats:
-                        if f.get('ext') == 'mp3':  # Filtra por mp3
+                        # En lugar de filtrar solo por mp3, toma cualquier formato de audio
+                        if f.get('vcodec') == 'none':  # Asegura que es solo audio
                             audio_url = f.get('url')
-                            break  # Sal del bucle una vez que encuentres el primer mp3
+                            break
 
             return audio_url
+    except yt_dlp.utils.DownloadError as e:
+        if "captcha" in str(e).lower():
+            print("Se requiere captcha para este video. No se puede obtener el audio.")
+        else:
+            print(f"Error inesperado: {e}")
+        return None
     except Exception as e:
         print(f"Error al extraer la URL: {e}")
         return None
--- e.py
+++ e.py
@@ -2,10 +2,8 @@ import requests
 json = {"user_id": "487430318500872203", 
         "channel_id": "1117201886836162630", 
         "guild_id": "1077968892535775262", 
-        "query": "tv gir not allowed"}
-req = "https://f1bf012d-c513-4256-8dae-351f7622d10e-00-124jjeazni7de.worf.replit.dev"
-if not req:
-    print("none")
-url  = requests.post("https://f1bf012d-c513-4256-8dae-351f7622d10e-00-124jjeazni7de.worf.replit.dev/play-music/", json=json)
+        "query": "zoe azul"}
 
-print(url.json())
+url  = requests.post("https://musicbdfd.onrender.com/play-music/", json=json)
+
+print(url.text)
