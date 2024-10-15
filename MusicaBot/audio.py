648ba52442dd57eb491a1ed2d169ad11d01a2056
Quab <148601206+quabwww@users.noreply.github.com>
2024-10-15 20:50:17 +0200

    Update audio.py

--- MusicaBot/audio.py
+++ MusicaBot/audio.py
@@ -2,7 +2,7 @@ import yt_dlp
 from pytube import YouTube
 #s
 def get_youtube_audio_url(video_url):
-    yt = YouTube(url)
+    yt = YouTube(video_url)
     audio_stream = yt.streams.filter(only_audio=True).first()
     print(audio_stream)
     return audio_stream.url
